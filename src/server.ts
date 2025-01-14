e9e778e7c84e314b16ac94f4cee2648a56153af6
srs-shihab <sabbir.reza@bevycommerce.com>
2025-01-14 17:25:51 +0600

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,70 +1,35 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import { PORT, DB_URL } from './config';
+import { logger, errorLogger } from './shared/logger';
 import { Server } from 'http';
-import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { requestLogger, logger, errorLogger } from './shared/logger';
-import routes from './router';
-import { handleNoRouteFound } from './errors/handleNoRouteFound';
 import { serverExitHandler } from './errors/serverExitHandler';
 import { initSocket } from './socket';
-import { PORT, DB_URL } from './config';
-
-const app: Application = express();
-
-// Middleware setup
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(requestLogger);
-
-// Health check route
-app.get('/', (req: Request, res: Response) => {
-  res.send('Event Ease Auth Server Running !');
-});
-
-// API routes
-app.use('/api/v1', routes);
-
-// Test route
-app.get('/test', async (req: Request, res: Response, next: NextFunction) => {
-  return res.send('Test is okay!');
-});
-
-// Global error handler
-app.use(globalErrorHandler);
-
-// Handle no route found error
-app.use(handleNoRouteFound);
-
-let server: Server;
+import app from './app';
 
 process.on('uncaughtException', err => {
   logger.error(err);
   process.exit(1);
 });
 
-async function connectDbAndStartServer() {
+let server: Server;
+
+async function connectDb() {
   try {
     await mongoose.connect(`${DB_URL}`);
     logger.info('Database connection established !');
-
     server = app.listen(PORT, () => {
       logger.info(`App is listening on port ${PORT}`);
     });
-
     initSocket(server);
   } catch (error) {
     errorLogger.error('Failed to connect database', error);
   }
-
   process.on('unhandledRejection', err => {
     serverExitHandler(server, err);
   });
 }
 
-connectDbAndStartServer();
+connectDb();
 
 process.on('SIGTERM', () => {
   if (server) {
